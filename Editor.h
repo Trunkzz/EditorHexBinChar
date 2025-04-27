4d3dd384296d01b6906d9879b915aba86fb0437c
Trunkzz <welterrobin98@gmail.com>
2025-04-27 14:11:41 +0200

    Update Editor.h

--- Editor.h
+++ Editor.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <string>
+
+// Klasse für die Bearbeitung der einzelnen Bytes
 class Editor {
 public:
     bool editByte(std::vector<unsigned char>& data, size_t position, const std::string& newValue, const std::string& mode);
