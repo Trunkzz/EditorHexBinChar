bb9a6bb6dabeaec2f256ba4d0078f52e584dc98d
Trunkzz <welterrobin98@gmail.com>
2025-04-27 14:08:37 +0200

    Update DisplayManager.h

--- DisplayManager.h
+++ DisplayManager.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+
+// Klasse für die Darstellung der Datei
 class DisplayManager {
 public:
     void printPage(const std::vector<unsigned char>& data, size_t offset, size_t width = 16, size_t height = 8);
