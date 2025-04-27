aa569bd9c15998873d5167ef99dc4fa8d34e9360
Trunkzz <welterrobin98@gmail.com>
2025-04-27 14:06:30 +0200

    Update Controller.h

--- Controller.h
+++ Controller.h
@@ -2,6 +2,8 @@
 #include "FileManager.h"
 #include "DisplayManager.h"
 #include "Editor.h"
+
+// Controller: Steuert den Ablauf der Anwendung
 class Controller {
 public:
     void run(const std::string& filename);
